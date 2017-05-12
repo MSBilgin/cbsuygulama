7cd8ede5cad7936cbda24e82bba1e2a047ad15ba
Mehmet Selim BILGIN <mselimbilgin@yahoo.com>
2017-05-12 10:45:38 +0300

    Create GooglePolylineDecoder.cs

---
+++ GooglePolylineDecoder.cs
@@ -0,0 +1,51 @@
+public List<double> PolylineCozumleyici(string polyline)
+        {
+            if (polyline == null || polyline == "") return null;
+
+            char[] polylinechars = polyline.ToCharArray();
+            int index = 0;
+            List<double> points = new List<double>();
+            int currentLat = 0;
+            int currentLng = 0;
+            int next5bits;
+            int sum;
+            int shifter;
+
+            while (index < polylinechars.Length)
+            {
+                // calculate next latitude
+                sum = 0;
+                shifter = 0;
+                do
+                {
+                    next5bits = (int)polylinechars[index++] - 63;
+                    sum |= (next5bits & 31) << shifter;
+                    shifter += 5;
+                } while (next5bits >= 32 && index < polylinechars.Length);
+
+                if (index >= polylinechars.Length)
+                    break;
+
+                currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+
+                //calculate next longitude
+                sum = 0;
+                shifter = 0;
+                do
+                {
+                    next5bits = (int)polylinechars[index++] - 63;
+                    sum |= (next5bits & 31) << shifter;
+                    shifter += 5;
+                } while (next5bits >= 32 && index < polylinechars.Length);
+
+                if (index >= polylinechars.Length && next5bits >= 32)
+                    break;
+
+                currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+
+                points.Add(Convert.ToDouble(currentLat) / 100000.0);
+                points.Add(Convert.ToDouble(currentLng) / 100000.0);
+            }
+
+            return points;
+        }
